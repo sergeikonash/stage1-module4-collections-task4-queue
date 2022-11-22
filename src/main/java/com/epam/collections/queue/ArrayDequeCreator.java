cf01b08dacb34b666b7bb00dd15952fc8129e160
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-22 13:05:42 +0100

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -8,20 +8,18 @@ import java.util.Queue;
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
         ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-        ArrayDeque<Integer> arrayDeque1 = (ArrayDeque<Integer>) firstQueue;
-        ArrayDeque<Integer> arrayDeque2 = (ArrayDeque<Integer>) secondQueue;
         arrayDeque.add(firstQueue.remove());
         arrayDeque.add(firstQueue.remove());
         arrayDeque.add(secondQueue.remove());
         arrayDeque.add(secondQueue.remove());
         try {
         while (firstQueue.size() > 0 && secondQueue.size() > 0) {
-            arrayDeque1.addLast(arrayDeque.pollLast());
-            arrayDeque.add(arrayDeque1.pollFirst());
-            arrayDeque.add(arrayDeque1.pollFirst());
-            arrayDeque2.addLast(arrayDeque.pollLast());
-            arrayDeque.add(arrayDeque2.pollFirst());
-            arrayDeque.add(arrayDeque2.pollFirst());
+            firstQueue.add(arrayDeque.pollLast());
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(firstQueue.poll());
+            secondQueue.add(arrayDeque.pollLast());
+            arrayDeque.add(secondQueue.poll());
+            arrayDeque.add(secondQueue.poll());
         }
         } catch (NoSuchElementException e) {
             e.printStackTrace();
